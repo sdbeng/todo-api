57670950e0fc2f0bae679d6c5bd9cadbc8a1cef1
Sergio Barrera <sdbeng@gmail.com>
2016-11-26 16:44:42 -0800

    Add svg icons to individual todo items

--- views/js/todo.js
+++ views/js/todo.js
@@ -1,19 +1,39 @@
-console.log('todo js starting');
 
 
+function go(){
 
-document.getElementById('add').addEventListener('click', function(){
-  let value = document.getElementById('todo-item').value;
-  console.log(value);
+  document.getElementById('add').addEventListener('click', function(){
+    let value = document.getElementById('todo-item').value;
+    console.log(value);
 
-  if(value){
-    addItemTodo(value);
-    document.getElementById('todo-item').value = '';
+    if(value){
+      addItemTodo(value);
+      document.getElementById('todo-item').value = '';
+    }
+
+  })
+
+  //addItemTodo function
+  function addItemTodo(text){
+    console.log('addItemTodo called');
+    var list = document.getElementById('todo');
+
+		var item = document.createElement('li');
+		item.innerText = text;
   }
 
-})
+  //
+}
 
-//addItemTodo function
-function addItemTodo(){
-  console.log('todo item added');
+
+
+
+
+///////////////////////////////////////////////////////
+/* THIS IS VERY IMPORTANT */
+//as soon as the DOM is ready, we'll execute the go() function
+if(document.readyState !== 'loading'){
+  go()
+}else {
+  document.addEventListener('DOMContentLoaded', go)
 }
